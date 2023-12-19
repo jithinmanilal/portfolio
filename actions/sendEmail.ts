b69b6af335b9d00227954d4a6f12ea204e76421a
Jithin Manilal <jithinmanilal@gmail.com>
2023-12-19 16:01:52 +0530

    Build corrected

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -3,6 +3,7 @@ import React from "react";
 import { getErrorMessage, validateString } from "@/lib/utils";
 import { Resend } from "resend";
 import ContactFormEmail from "@/email/contact_form_email";
+import { renderAsync } from "@react-email/components";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -30,17 +31,20 @@ export async function sendEmail(formData: FormData) {
   }
 
   try {
+    const html = await renderAsync(
+      React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      })
+    );
+
     await resend.emails.send({
       from: `Portfolio site mail <onboarding@resend.dev>`,
       to: 'jithinmanilal736@gmail.com',
       subject: messageSubject as string,
       reply_to: senderEmail as string,
-      // text: message as string,
-      react: React.createElement(ContactFormEmail, {
-        message: message as string,
-        senderEmail:senderEmail as string,
-      }),
-    })
+      html: html,
+    });
   }
   catch (error) {
     return {
